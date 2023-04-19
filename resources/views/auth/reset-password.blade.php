d2c16248644cc13f34c751877f1bf8efb0728742
Yann Boyongo <yaboyon3@gmail.com>
2023-04-19 15:06:46 +0200

    changing layout for reset password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,44 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+@extends('layouts.auth.app')
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+@section('content')
+    <div class="card">
+        <div class="card-body login-card-body">
+            <form action="{{ route('password.store') }}" method="POST">
+                <!-- Password Reset Token -->
+                @csrf
+                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                <div class="mb-3">
+                    <input type="email" name="email" value="{{ old('email', $request->email) }}" class="form-control"
+                        placeholder="Email" autofocus>
+                    @error('email')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+                </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <div class="mb-3">
+                    <input type="password" name="password" class="form-control" placeholder="{{ __('Mot de passe') }}"
+                        required>
+                    @error('password')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <input type="password" name="password_confirmation" class="form-control"
+                        placeholder="{{ __('Confirmer mot de passe') }}" required>
+                    @error('password_confirmation')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <button type="submit"
+                            class="btn btn-primary btn-block">{{ __('réinitialiser le mot de passe') }}</button>
+                    </div>
+                </div>
+
+            </form>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+@endsection
