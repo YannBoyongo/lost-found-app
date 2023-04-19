25774af5ba9ae359f4c74b432af6c64b05556893
Yann Boyongo <yaboyon3@gmail.com>
2023-04-19 15:04:56 +0200

    updating login and forgot password layout

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,33 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@extends('layouts.auth.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-body login-card-body">
+            <p class="login-box-msg">
+                {{ __('Vous avez oublié votre mot de passe ? Indiquez-nous votre adresse électronique et nous vous enverrons un lien de réinitialisation du mot de passe') }}
+            </p>
+
+            <form action="{{ route('password.email') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <input type="email" name="email" class="form-control" placeholder="Email" autofocus>
+                    @error('email')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <button type="submit" class="btn btn-primary btn-block">{{ __('Envoyer lien') }}</button>
+                    </div>
+                </div>
+
+            </form>
+
+            <p class="mb-1">
+                <a href="{{ route('login') }}">{{ __('Vous êtes déjà inscrit ?') }}</a>
+            </p>
+
+        </div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+@endsection
--- resources/views/layouts/auth/app.blade.php
+++ resources/views/layouts/auth/app.blade.php
@@ -24,51 +24,7 @@
                 @endif
             </a>
         </div>
-        <div class="card">
-            <div class="card-body login-card-body">
-                <p class="login-box-msg">{{ __('Se connecter pour commencer votre session') }}</p>
-
-                <form action="{{ route('login') }}" method="POST">
-                    @csrf
-                    <div class="mb-3">
-                        <input type="email" name="email" class="form-control" placeholder="{{ __('Email') }}"
-                            autofocus>
-                        @error('email')
-                            <span class="error">{{ $message }}</span>
-                        @enderror
-                    </div>
-
-                    <div class="mb-3">
-                        <input type="password" name="password" class="form-control"
-                            placeholder="{{ __('Mot de passe') }}">
-                        @error('password')
-                            <span class="error">{{ $message }}</span>
-                        @enderror
-                    </div>
-
-                    <div class="row">
-                        <div class="col-12 mb-2">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember" name="remember">
-                                <label for="remember">
-                                    {{ __('Se souvenir de moi') }}
-                                </label>
-                            </div>
-                        </div>
-                        <div class="col-12 mb-3">
-                            <button type="submit" class="btn btn-primary btn-block">{{ __('Se connecter') }}</button>
-                        </div>
-                    </div>
-                </form>
-
-                <p class="mb-1">
-                    <a href="{{ route('password.request') }}">{{ __('Mot de passe oublié') }}</a>
-                </p>
-                <p class="mb-0">
-                    <a href="{{ route('register') }}" class="text-center">{{ __('Créer un nouveau compte') }}</a>
-                </p>
-            </div>
-        </div>
+        @yield('content')
     </div>
     <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
