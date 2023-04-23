898ebe80fe483276d78e6d4ea4641d7d1b5d6263
Yann Boyongo <yaboyon3@gmail.com>
2023-04-23 10:00:06 +0200

    adding delete password form

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -21,12 +21,12 @@
         <div class="col-md-8">
             <div class="card card-primary">
                 <div class="card-header">
-                    <h3 class="card-title">{{ __("Update your account's profile information and email address.") }}</h3>
+                    <h3 class="card-title">{{ __('Information du profil') }}</h3>
                 </div>
 
-                <form action="" method="POST">
+                <form action="{{ route('profile.update') }}" method="POST">
                     @csrf
-                    @method('PUT')
+                    @method('patch')
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-12">
@@ -57,7 +57,7 @@
                     <div class="card-footer d-flex">
                         <div class="flex-grow-1 text-right">
                             <button type="submit" class="btn btn-primary">
-                                {{ _('Enregister') }}
+                                {{ _('Modifier') }}
                                 <i class="fa fa-save ml-2"></i>
                             </button>
                         </div>
@@ -71,20 +71,21 @@
         <div class="col-md-8">
             <div class="card card-primary">
                 <div class="card-header">
-                    <h3 class="card-title">{{ __('Ensure your account is using a long, random password to stay secure.') }}
+                    <h3 class="card-title">{{ __('Modifier mot de passe') }}
                     </h3>
                 </div>
 
-                <form action="" method="POST">
+                <form action="{{ route('password.update') }}" method="POST">
                     @csrf
-                    @method('PUT')
+                    @method('put')
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <label for="current_password">{{ __('Mot de passe actuel') }}</label>
-                                    <input type="password" class="form-control" placeholder="{{ __('Entrer le nom') }}"
-                                        name="current_password" id="current_password" value="">
+                                    <input type="password" class="form-control"
+                                        placeholder="{{ __('Mot de passe actuel') }}" name="current_password"
+                                        id="current_password" value="">
                                     @error('current_password')
                                         <span class="error">{{ $message }}</span>
                                     @enderror
@@ -103,12 +104,25 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="password_confirmation">{{ __('Confirmer nouveau Mot de passe') }} </label>
+                                    <input type="password" class="form-control"
+                                        placeholder="{{ __('Entrer password_confirmation') }}" name="password"
+                                        id="password_confirmation" value="">
+                                    @error('password_confirmation')
+                                        <span class="error">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                     <div class="card-footer d-flex">
                         <div class="flex-grow-1 text-right">
                             <button type="submit" class="btn btn-primary">
-                                {{ _('Enregister') }}
+                                {{ _('Modifier') }}
                                 <i class="fa fa-save ml-2"></i>
                             </button>
                         </div>
@@ -122,43 +136,34 @@
         <div class="col-md-8">
             <div class="card card-primary">
                 <div class="card-header">
-                    <h3 class="card-title">{{ __("Update your account's profile information and email address.") }}</h3>
+                    <h3 class="card-title">{{ __('Supprimer compte') }}</h3>
                 </div>
-
-                <form action="" method="POST">
+                <form action="{{ route('profile.destroy') }}" method="POST">
                     @csrf
-                    @method('PUT')
+                    @method('delete')
                     <div class="card-body">
+                        <p>Une fois votre compte supprimé, toutes ses ressources et données seront définitivement
+                            supprimées. Avant
+                            de supprimer votre compte, veuillez télécharger les données ou informations que vous souhaitez
+                            conserver.</p>
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group">
-                                    <label for="name">{{ __('Nom') }}</label>
-                                    <input type="text" class="form-control" placeholder="{{ __('Entrer le nom') }}"
-                                        name="name" id="name" value="{{ old('name', Auth::user()->name) }}">
-                                    @error('name')
+                                    <label for="password">{{ __('Nouveau Mot de passe') }} </label>
+                                    <input type="password" class="form-control"
+                                        placeholder="{{ __('Entrer password') }}" name="password" id="password"
+                                        value="">
+                                    @error('password')
                                         <span class="error">{{ $message }}</span>
                                     @enderror
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label for="email">{{ __('Source powerbie') }} </label>
-                                    <input type="text" class="form-control" placeholder="{{ __('Entrer Email') }}"
-                                        name="email" id="email" value="{{ old('email', Auth::user()->email) }}">
-                                    @error('email')
-                                        <span class="error">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
                     <div class="card-footer d-flex">
                         <div class="flex-grow-1 text-right">
                             <button type="submit" class="btn btn-primary">
-                                {{ _('Enregister') }}
+                                {{ _('Supprimer mon compte') }}
                                 <i class="fa fa-save ml-2"></i>
                             </button>
                         </div>
